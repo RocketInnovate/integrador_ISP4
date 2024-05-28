4010e8dda645fa9664d37ed5c8cb43a65ca664fe
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2024-05-27 23:31:34 -0300

    Update build.gradle.kts

--- PassKeeper/app/build.gradle.kts
+++ PassKeeper/app/build.gradle.kts
@@ -34,13 +34,14 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
+    //noinspection GradleDependency
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.github.f0ris.sweetalert:library:1.5.6") //Para las alertas de Sweet Alert
     implementation ("org.mindrot:jbcrypt:0.4") //para hashing
-    implementation ("androidx.biometric:biometric:1.2.0-alpha05") //Para la autenftificación biometrica
+    implementation ("androidx.biometric:biometric:1.2.0-alpha05") //Para la autentificación biometrica
+    implementation("com.squareup.okhttp3:okhttp:4.12.0") //Para hacer consultas a una API externa
 }
